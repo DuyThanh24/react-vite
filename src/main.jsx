dd9baea881de761a039af4fa492474efceb27397
Macbook <macbook@MacBook-Air-cua-Macbook.local>
2024-06-20 23:24:34 +0700

    video 45

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    {/* <RouterProvider router={router} /> */}
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
